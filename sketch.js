c6def927fb53690c5ba5229722e659a635725048
Aditya Chettri <drumsapkota@gmail.com>
2017-05-28 10:11:49 +0530

    learning to draw basic shapes

--- sketch.js
+++ sketch.js
@@ -1,12 +1,26 @@
 function setup(){
-	createCanvas(640,400);
+	createCanvas(740,400);
+	background(200);
+
+	//set colors
+	fill(204,101,192,127);
+	stroke(127,63,120);
+
+	//a rectangle
+	rect(40,120,120,40);
+
+	//an ellipse
+	ellipse(240,240,80,80);
+
+	//a triangle
+	triangle(300,100,320,100,310,80);
+
+	//designn of a simple flower
+	translate(580,200);
+	noStroke();
+	for(var i=0; i<10; i++){
+		ellipse(0,30,20,80);
+		rotate(PI/5);
+	}
 }
 
-function draw(){
-	if(mouseIsPressed){
-		fill(0);
-	}else{
-		fill(255);
-	}
-	ellipse(mouseX,mouseY, 80,80)
-}
