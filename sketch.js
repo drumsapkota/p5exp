93769cbd7bc3602e8cdb00e0be28d5ea7a4b03e0
Aditya Chettri <drumsapkota@gmail.com>
2017-05-30 23:38:16 +0530

    sounds exciting

--- sketch.js
+++ sketch.js
@@ -1,18 +1,25 @@
 
 
 function setup(){
-	createCanvas(480,120);
-	//fill(255);
-	//stroke(102);
+	createCanvas(480,440);
+	background(0);
+	//create a audio input and start it
+	mic = new p5.AudioIn();
+	mic.start();
+	//create a new amplitude analyzer and patch input into it
+	amp = new p5.Amplitude();
+	amp.setInput(mic);
+
 }
 
 function draw(){
-	background(0);
-	for(var y = 20; y <= height; y+=8){
-		for(var x = 12; x <= width; x+=15){
-			ellipse(x+y,y,16-y/10.0,16-y/10.0);
-			
-		}
-	}
+	noStroke();
+	fill(12,12,12,10);
+	rect(0,0,width,height);
+	scale = map(amp.getLevel(),0,1.0,10,width);
+	//draw the circle based on volume
+	fill(255);
+	newScale = random(scale, scale *30);
+	ellipse(width/2,height/2, newScale, newScale);
 }
 
