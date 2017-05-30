ab6866bdf5ed9f83378d30b8c3596063688f9994
Aditya Chettri <drumsapkota@gmail.com>
2017-05-30 22:46:35 +0530

    Shiny disco lights

--- sketch.js
+++ sketch.js
@@ -2,16 +2,18 @@
 
 function setup(){
 	createCanvas(480,420);
-	strokeWeight(2);
+	noStroke();
+	//strokeWeight(2);
 }
 
 function draw(){
-	background(204);
+	background(0);
 	
-	//for(var i= 20, i < 400, i += 8){
-	for( i = 20; i < 400; i = i+ 10){	
-		line(20,40, random(110,140), 180 + random (1,120));
+	for(var y = 0; y<= height; y+=40){
+		for(var x = 0; x <= width;  x+=40){
+			fill(random(0,255),random(0,255),random(0,255),);
+			ellipse(x,y,40,40);
+		}
 	}
-
 }
 
