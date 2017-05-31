696c62bed6e644106b38a34f3de49e3e10c36d0e
Aditya Chettri <drumsapkota@gmail.com>
2017-05-31 06:41:16 +0530

    accessing the webcam

--- sketch.js
+++ sketch.js
@@ -1,26 +1,16 @@
-var sine;
-var freq = 400;
+var capture;
 
 function setup(){
 	createCanvas(440,440);
-	//create and start the sine oscillator
-	sine = new p5.SinOsc();
-	sine.start();
-
+	capture = createCapture();
+	capture.hide();
 }
 
 function draw(){
-	background(0);
-	//map the mouseX value for the value of freq
-	var hertz = map(mouseX,0,width,20.0,440.0);
-	sine.freq(hertz);
-	//Draw a wave to visualize the freq of the sound
-	stroke(204);
-	for(var x = 0 ;x < width; x ++){
-		var angle = map(x, 0, width, 0, TWO_PI * hertz);
-		var sineValue = sin(angle)*120;
-		line(x,0,x, height/2 + sineValue);
-	}
+	var aspectRatio = capture.height / capture.width;
+	var h = width * aspectRatio;
+	image(capture,0,0,width,h);
+	//filter(INVERT);
 	
 }
 
